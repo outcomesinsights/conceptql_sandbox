65c3273c7feb2f61536226c8717552e7da3d9f99
Ryan Duryea <aguynamedryan@gmail.com>
2014-08-25 11:01:51 -0700

    convert: update to use all but first comment line as description

--- convert.rb
+++ convert.rb
@@ -7,7 +7,7 @@ Pathname.new('statements').children.each do |child|
     lines = file.readlines
     attribs = {
       title: lines.first.sub(/^#\s*/, ''),
-      description: lines.select { |l| l =~ /^#/ }.map { |l| l.sub(/^#\s*/, '') }.join("\n"),
+      description: lines.select { |l| l =~ /^#/ }.map { |l| l.sub(/^#\s*/, '') }[1..99].join("\n"),
       statement: (eval(lines.select { |l| l !~ /^#/ }.join("\n"))).to_json
     }
     Example.find_or_create(attribs)
