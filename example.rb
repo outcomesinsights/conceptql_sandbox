1bbe7d24e793162d061df1cc0c80fb68fa1d1e26
Ryan Duryea <aguynamedryan@gmail.com>
2014-08-28 17:31:09 -0700

    Example: update to take and pass dialect all around

--- example.rb
+++ example.rb
@@ -15,16 +15,16 @@ class Example < Sequel::Model
     hash_it(id)
   end
 
-  def sql
-    query.sql
+  def sql(dialect)
+    query(dialect).sql
   rescue LoadError
     "Statement includes experimental nodes.  Cannot generate SQL statement."
   end
 
-  def image_path
-    output_file = Pathname.new('public') + file_hash
+  def image_path(dialect)
+    output_file = Pathname.new('public') + file_hash(dialect)
     file = Pathname.new(output_file.to_s + '.png')
-    graph_it(parsed_statement, output_file.to_s) unless file.exist?
+    graph_it(dialect, parsed_statement, output_file.to_s) unless file.exist?
     file.basename.to_s
   end
 
@@ -36,48 +36,48 @@ class Example < Sequel::Model
     @yaml_statement ||= parsed_statement.to_yaml
   end
 
-  def partial_results
-    @partial_results ||= read_or_create_results_file
+  def partial_results(dialect)
+    @partial_results ||= read_or_create_results_file(dialect)
   end
 
   private
-  def data_db
-    @data_db ||= Sequel.connect(Psych.load_file('config/database.yml')['data'])
+  def data_db(dialect)
+    @data_db ||= Sequel.connect(database_config(dialect))
   end
 
-  def query
-    @query ||= ConceptQL::Query.new(data_db, parsed_statement)
+  def query(dialect)
+    @query ||= ConceptQL::Query.new(data_db(dialect), parsed_statement)
   end
 
-  def graph_it(statement, file)
+  def graph_it(dialect, statement, file)
     ConceptQL::Graph.new(statement,
                          dangler: true,
                          suffix: 'png',
-                         db: data_db
+                         db: data_db(dialect)
                         ).graph_it(file)
   rescue LoadError
     ConceptQL::FakeGrapher.new(suffix: 'png').graph_it(statement, file)
   end
 
-  def file_hash
-    @file_hash ||= Digest::SHA256.hexdigest([ statement, sql, database_config ].join)
+  def file_hash(dialect)
+    @file_hash ||= Digest::SHA256.hexdigest([ statement, sql(dialect), database_config(dialect) ].join)
   end
 
-  def database_config
-    Psych.load_file('config/database.yml')['data']
+  def database_config(dialect)
+    @database_config ||= Psych.load_file('config/database.yml')[dialect]
   end
 
-  def read_or_create_results_file
+  def read_or_create_results_file(dialect)
     results_dir = Pathname.new('results')
     results_dir.mkdir unless results_dir.exist?
 
-    file_name = file_hash + '.json'
+    file_name = file_hash(dialect) + '.json'
     results_file = results_dir + file_name
 
     return JSON.parse(results_file.read) if results_file.exist?
 
     results = begin
-      query.query.from_self.limit(10).all
+      query(dialect).query.from_self.limit(10).all
     rescue LoadError
       [ { error: 'Query contains experimental nodes.  Cannot fetch results.' } ]
     end
